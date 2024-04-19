816dcdd7a17da293b1c2631b4ab4cad6cb8d064d
roger20191106 <79368866+roger20191106@users.noreply.github.com>
2024-04-19 09:28:14 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 //打印内容
-def PrintMes(content，color){
+def PrintMes(content,color){
     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'blue'  : "\033[47;34m ${value} \033[0m",
               'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
